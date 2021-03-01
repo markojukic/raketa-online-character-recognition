06404ba4fb6e936f10511aa1895f764d93e715a0
markojukic <jukic.marko998@gmail.com>
2021-03-01 20:14:51 +0100

    sredio kod

--- classifier_pickle.py
+++ classifier_pickle.py
@@ -3,41 +3,63 @@ import preprocessing
 from drawing import Drawing
 from dataclasses import dataclass
 import dtw
-import pickle
+from joblib import dump
 from typing import Any
-
-
-def load(filename):
-    with open(filename, 'rb') as f:
-        return pickle.load(f)
+from preprocessing import DrawingTransformer
+from hosvdclassifier import HOSVDClassifier
 
 
 class ClassifierPickle:
     def save(self, filename):
-        with open(filename, 'wb') as f:
-            pickle.dump(self, f)
+        dump(self, filename)
 
 
 @dataclass
-class DTWClassifierPickle(ClassifierPickle):
+class KNNDTWPickle(ClassifierPickle):
     stacked_train: np.ndarray
     drawing_scaler: preprocessing.DrawingToBoxScaler
     drawing_resampler: preprocessing.DrawingResampler
     cls: Any  # Sklearn classifier
 
+    def get_X(self, drawing: Drawing):
+        return dtw.dtw_distance_matrix(np.vstack(drawing.strokes)[None, :], self.stacked_train)
+
     def predict(self, drawing: Drawing):
         drawing = self.drawing_scaler.transform_one(drawing)
         drawing = self.drawing_resampler.transform_one(drawing)
-        d = dtw.dtw_distance_matrix(np.vstack(drawing.strokes)[None, :], self.stacked_train)
-        return self.cls.predict(d)[0]
+        return self.cls.predict(self.get_X(drawing))[0]
 
 
 @dataclass
-class RSIDTWClassifierPickle(DTWClassifierPickle):
+class KNNRSIDTWPickle(KNNDTWPickle):
     n_iter: int
 
+    def get_X(self, drawing: Drawing):
+        return dtw.rsi_dtw_distance_matrix(np.vstack(drawing.strokes)[None, :], self.stacked_train, self.n_iter)
+
+
+@dataclass
+class SVMDTWPickle(KNNDTWPickle):
+    gamma: float
+
+    def get_X(self, drawing: Drawing):
+        return np.exp(-self.gamma * super().get_X(drawing))
+
+
+@dataclass
+class SVMRSIDTWPickle(KNNRSIDTWPickle):
+    gamma: float
+
+    def get_X(self, drawing: Drawing):
+        return np.exp(-self.gamma * super().get_X(drawing))
+
+
+@dataclass
+class HOSVDPickle(ClassifierPickle):
+    transformers: list[DrawingTransformer]
+    cls: HOSVDClassifier
+
     def predict(self, drawing: Drawing):
-        drawing = self.drawing_scaler.transform_one(drawing)
-        drawing = self.drawing_resampler.transform_one(drawing)
-        d = dtw.rsi_dtw_distance_matrix(np.vstack(drawing.strokes)[None, :], self.stacked_train, self.n_iter)
-        return self.cls.predict(d)[0]
+        for transformer in self.transformers:
+            drawing = transformer.transform_one(drawing)
+        return self.cls.predict(drawing[None, :])[0]
